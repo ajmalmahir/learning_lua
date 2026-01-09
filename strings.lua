00c774f30dd08bf21f5c681c6b47fa18fab685cc
unknown <ajmalmahir@icloud.com>
2026-01-09 22:21:17 +0000

    more strings

--- strings.lua
+++ strings.lua
@@ -3,3 +3,23 @@ a = "one string"
 b = string.gsub(a, "one", "another") -- change string parts
 print(a)
 print(b)
+
+-- delimit literal strings by matching single or double quotes
+a = "a line"
+b = 'another line'
+
+-- escape sequences
+print("\a bell")
+print("\b backspace")
+print("\f form feed")
+print("\n newline")
+print("\r carriage return")
+print("\t horizontal tab")
+print("\v vertical tab")
+print("\\ backslash")
+print("\" double quote")
+print("\' single quote")
+
+print("one line\nnext line\n\"in quotes\", 'in quotes'")
+print('a backslash inside quotes: \'\\\'')
+print("a simpler way: '\\'")
